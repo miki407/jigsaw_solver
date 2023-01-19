8c48c497c505cd21d41e01debb191a42b01914f6
laki407 <123081653+laki407@users.noreply.github.com>
2023-01-19 21:18:53 +0100

    jpg to bmp

--- Preprocessing/Preprocessing/Preprocessing.cpp
+++ Preprocessing/Preprocessing/Preprocessing.cpp
@@ -1,9 +1,14 @@
-// Preprocessing.cpp : This file contains the 'main' function. Program execution begins and ends there.
+#include <opencv2/opencv.hpp>
 
-#include <iostream>
+int main(int argc, char** argv) {
+    // Load the image
+    cv::Mat image = cv::imread("C:\\Users\\a\\source\\repos\\jigsaw_solver\\Test_Image\\Test_Image.jpg");
 
-int main()
-{
-    std::cout << "Hello World!\n";
+    // Convert the image to grayscale
+    cv::Mat gray;
+    cv::cvtColor(image, gray, cv::COLOR_BGR2GRAY);
 
+    // Save the grayscale image as a bitmap
+    cv::imwrite("C:\\Users\\a\\source\\repos\\jigsaw_solver\\Test_Image\\Test_Image.bmp", gray);
+    return 0;
 }
